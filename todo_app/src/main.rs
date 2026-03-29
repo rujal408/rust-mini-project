629b1ae7fd6cb7fe67ba35376dc0e44058e30238
rujal408 <rujal408@gmail.com>
2026-03-29 21:05:31 +0545

    update: add todo in file

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -1,15 +1,40 @@
 use std::env;
+use std::fs;
+use std::fs::OpenOptions;
+use std::io::Write;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
+    if args.len() < 2 {
         println!("Usage: cargo run <command> <value>");
         return;
     }
-    let command = &args[1];
-    let value = &args[2];
+    let command = args[1].as_str();
 
-    println!("Command {}", command);
-    println!("Value {}", value);
+    match command {
+        "add" => {
+            if args.len() < 3 {
+                println!("Please provide a task to add");
+                return;
+            }
+            let value = &args[2];
+
+            let mut file = OpenOptions::new()
+                .append(true)
+                .create(true)
+                .open("todos.txt")
+                .expect("Failed to open file");
+
+            writeln!(file, "{}", value).expect("Failed to write");
+            println!("Task added: {}", value);
+        }
+        "list" => {
+            let content = fs::read_to_string("todos.txt").unwrap_or(String::from("No tasks found"));
+            println!("{}", content);
+        }
+
+        "delete" => {}
+        _ => println!("Unknown command"),
+    }
 }
