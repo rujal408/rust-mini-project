7f39cc2046e8996537fc9ec91e1262ca27af01de
rujal408 <rujal408@gmail.com>
2026-04-02 11:35:52 +0545

    update: update completed

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -8,6 +8,19 @@ struct Todo {
     completed: bool,
 }
 
+fn get_todos() -> Vec<Todo> {
+    let content = fs::read_to_string("todos.txt").unwrap_or(String::from("No tasks found"));
+    let mut todos: Vec<Todo> = Vec::new();
+
+    for line in content.lines() {
+        let completed = line.starts_with("[x]");
+        let text = line[4..].to_string();
+
+        todos.push(Todo { text, completed });
+    }
+    todos
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -37,15 +50,7 @@ fn main() {
             println!("Task added: {}", value);
         }
         "list" => {
-            let content = fs::read_to_string("todos.txt").unwrap_or(String::from("No tasks found"));
-            let mut todos: Vec<Todo> = Vec::new();
-
-            for line in content.lines() {
-                let completed = line.starts_with("[x]");
-                let text = line[4..].to_string();
-
-                todos.push(Todo { text, completed });
-            }
+            let todos = get_todos();
 
             for todo in &todos {
                 let status = if todo.completed { "[x]" } else { "[ ]" };
@@ -53,7 +58,33 @@ fn main() {
             }
         }
 
-        "delete" => {}
+        "delete" => {
+            if args.len() < 3 {
+                println!("Please provide a task to add");
+                return;
+            }
+        }
+        "complete" => {
+            if args.len() < 3 {
+                println!("Please provide task index");
+                return;
+            }
+            let todos = get_todos();
+            let index: usize = args[2].parse().expect("Invalid number");
+
+            let mut file = fs::File::create("todos.txt").expect("Failed to open file");
+            for (i, todo) in todos.iter().enumerate() {
+                if i == index {
+                    writeln!(file, "[x] {}", todo.text);
+                } else {
+                    if todo.completed == false {
+                        writeln!(file, "[ ] {}", todo.text);
+                    } else {
+                        writeln!(file, "[x] {}", todo.text);
+                    }
+                }
+            }
+        }
         _ => println!("Unknown command"),
     }
 }
