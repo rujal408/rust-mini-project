c142f4d8b8851885116f399d47aafaa14ff4ae3c
rujal408 <rujal408@gmail.com>
2026-04-02 11:04:21 +0545

    update: list

--- todo_app/src/main.rs
+++ todo_app/src/main.rs
@@ -3,8 +3,15 @@ use std::fs;
 use std::fs::OpenOptions;
 use std::io::Write;
 
+struct Todo {
+    text: String,
+    completed: bool,
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+
+    // let todos: Vec<Todo> = vec![];
 
     if args.len() < 2 {
         println!("Usage: cargo run <command> <value>");
@@ -26,12 +33,24 @@ fn main() {
                 .open("todos.txt")
                 .expect("Failed to open file");
 
-            writeln!(file, "{}", value).expect("Failed to write");
+            writeln!(file, "[ ] {}", value).expect("Failed to write");
             println!("Task added: {}", value);
         }
         "list" => {
             let content = fs::read_to_string("todos.txt").unwrap_or(String::from("No tasks found"));
-            println!("{}", content);
+            let mut todos: Vec<Todo> = Vec::new();
+
+            for line in content.lines() {
+                let completed = line.starts_with("[x]");
+                let text = line[4..].to_string();
+
+                todos.push(Todo { text, completed });
+            }
+
+            for todo in &todos {
+                let status = if todo.completed { "[x]" } else { "[ ]" };
+                println!("{} {}", status, todo.text);
+            }
         }
 
         "delete" => {}
